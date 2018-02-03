6a7b359cc36b032ad72e4ddb25dea39c97d807b9
MarcosMendes9389 <marcosmendes9389@gmail.com>
2018-02-03 12:01:26 -0200

    sample-client-service

---
+++ sample/src/main.js
@@ -0,0 +1,5 @@
+const foo = require("./mymodule1")
+const bar = require("./anothermodule").bar
+
+foo(2)
+console.log(bar(3))
---
+++ se05ep07-client/src/main.js
@@ -0,0 +1,13 @@
+// main.js
+require("./main.css")
+const Vue = require("vue")
+const VueMaterial = require("vue-material")
+
+Vue.use(VueMaterial)
+
+Vue.component("hello-vue", require("./hello.vue"))
+
+new Vue({
+  el: "#mountpoint",
+  render: r => r("hello-vue")
+})
